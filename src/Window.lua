60599b3ea1b85d4353ee27b109a59db37c0934ca
xxCichyxx <179588410+xxCichyxx@users.noreply.github.com>
2026-01-16 01:41:28 +0100

    xx

--- src/Window.lua
+++ src/Window.lua
@@ -65,8 +65,7 @@ function Window:Create(config)
     local MainFrame = Instance.new("Frame")
     MainFrame.Name = "MainFrame"
     MainFrame.Size = UDim2.new(0, 700, 0, 400)
-    MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-    MainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
+    MainFrame.Position = UDim2.new(0, 0, 0, 0)
     MainFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
     MainFrame.BorderSizePixel = 0
     MainFrame.ClipsDescendants = true
